a1d86a7d7cb72e0e3a057c2827514855d09dee7b
Bayron Puac <bayronpuac@gmail.com>
2019-08-20 22:29:41 -0500

    Updated the array.js file

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -93,7 +93,24 @@ console.log(ticketPriceTotal);
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+let email = [];
+runners.forEach(function(items){
+  return email.push(`${items.email}`);
+});
+
+console.log(email);
 
 // Problem 2
 
+let runnersCompanyName = runners.filter(function(items){
+  return items.company_name === "Skinix";
+});
+console.log(runnersCompanyName);
+
 // Problem 3
+
+
+let runnersDonation = runners.filter(function(items){
+  return items.donation > "200";
+});
+console.log(runnersDonation);
